00ad9b1dd3799da12a4c6ab7b29d7a51393a52a9
Paulina <paulina.czesia@wp.pl>
2024-11-29 10:09:53 +0100

    zmiana na master

--- src/main/java/eu/chrost/git/Main.java
+++ src/main/java/eu/chrost/git/Main.java
@@ -1,5 +1,7 @@
 package eu.chrost.git;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
@@ -7,5 +9,9 @@ public class Main {
         System.out.println("QWERTY");
         System.out.println("Zmiana GIT");
         System.out.println("Zmiana Intelij");
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        scanner.close();
     }
 }
