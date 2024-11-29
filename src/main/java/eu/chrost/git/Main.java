2444b6933282df9457a2c97ccf0ee7e10e2066c4
paulina-czesia <paulina.czesia@wp.pl>
2024-11-29 09:41:48 +0100

    Update Main.java

--- src/main/java/eu/chrost/git/Main.java
+++ src/main/java/eu/chrost/git/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Paulina");
         System.out.println("QWERTY");
+        System.out.println("Zmiana GIT")
     }
 }
