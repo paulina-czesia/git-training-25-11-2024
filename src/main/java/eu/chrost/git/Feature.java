787ab328cb1699b1f8f64a590ce32ba421a2c245
Paulina <paulina.czesia@wp.pl>
2024-11-29 10:17:33 +0100

    zmiana na feature 2

--- src/main/java/eu/chrost/git/Feature.java
+++ src/main/java/eu/chrost/git/Feature.java
@@ -3,5 +3,6 @@ package eu.chrost.git;
 public class Feature {
     public static void main(String[] args) {
         System.out.println("branch feature");
+        System.out.println("commit branch feature 2");
     }
 }
